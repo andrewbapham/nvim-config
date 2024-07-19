fa78c3fc6e5569c1618f4969c1d7774c5d034878
Andrew Pham <andrewbapham@outlook.com>
2024-07-19 16:49:53 -0400

    Add clangd lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -43,6 +43,7 @@ return {
         }
       })
       lspconfig.basedpyright.setup({})
+      lspconfig.clangd.setup({})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
